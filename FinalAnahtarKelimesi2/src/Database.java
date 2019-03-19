3482a3c0b30535f3ecacb1c0f0f00778126be3be
İbrahim Ali METİN <ibrahimalimetin@gmail.com>
2019-03-19 23:56:32 +0300

    Update Database.java

--- FinalAnahtarKelimesi2/src/Database.java
+++ FinalAnahtarKelimesi2/src/Database.java
@@ -2,10 +2,10 @@
  *
  * @author Ibrahim Ali Metin
  */
-public class Database { //Class 'ı fial yaparsak ondan hiçir alt sınıf üretemeyiz.
+public class Database { //Class 'ı final yaparsak ondan hiçbir alt sınıf üretemeyiz.
     
     public void baglantiKur(final String userName,final String password){ // Bu değişkenleri final yapmaz isek değişiklik yapılabilir bu sebeple final anahtar kelimesini kullanacağız.
-        //methodu final yaparsak bu classı extends ettiğimiz class içierisinde override edemeyiz.
+        //methodu final yaparsak bu classı extends ettiğimiz class içerisinde override edemeyiz.
         System.out.println(userName);
         System.out.println(password);
     }
